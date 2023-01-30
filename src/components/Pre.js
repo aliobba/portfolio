8620cb310cd29417933221b8dc99d44bccaacb9b
aliobba <ali.obba@esprit.tn>
2023-01-30 17:22:42 +0100

    lazy loading screen

--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,6 +1,32 @@
-import React from "react";
+/* import React from "react";
 function Pre(props) {
   return <div id={props.load ? "preloader" : "preloader-none"}></div>;
+}
+
+export default Pre;
+ */
+import React from "react";
+import "./Pre.css";
+function Pre(props) {
+  if (props.load) {
+    return (
+      <div className={props.load ? "body" : "pl-none"}>
+        <div className="pl">
+          <div className="pl__outer-ring"></div>
+          <div className="pl__inner-ring"></div>
+          <div className="pl__track-cover"></div>
+          <div className="pl__ball">
+            <div className="pl__ball-texture"></div>
+            <div className="pl__ball-outer-shadow"></div>
+            <div className="pl__ball-inner-shadow"></div>
+            <div className="pl__ball-side-shadows"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return <div className="pl-none"></div>;
 }
 
 export default Pre;
